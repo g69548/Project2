2b56ad4d562ced11ebb3d8760bb524b0c6546cb9
Mitchell VanBurenII <vanburen14@fullsail.edu>
2013-03-14 18:41:45 -0400

    code first commit

--- VanBurenII_Mitchell_Assignment2.js
+++ VanBurenII_Mitchell_Assignment2.js
@@ -1,1 +1,13 @@
 //alert("JavaScript works!");
+
+/*
+ * Mitchell VanBurenII
+ * SDI Assignment 2
+ * 1303
+ *
+ * This code will be just a more indepth and none static way to lower your car with
+ * the brand coilovers and the amount that is preffered by the mechanic and why it is
+ * preffered.  */
+
+var mechanic = Mitchell;
+
